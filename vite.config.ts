4aabf1a1352bc26d52d1b9276f8cadf8d62a3ff1
ragul <pradhivetommy@gmail.com>
2026-06-30 18:50:36 +0530

    Rename project to ragul-portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/preset-sites/jack-3d-creator/',
+  base: '/preset-sites/ragul-portfolio/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
